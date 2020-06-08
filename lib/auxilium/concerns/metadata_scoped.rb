72782e428fbf3e9a669a37d2f2b384500b232197
Mark Cornelissen <mark@boxture.com>
2020-06-08 08:28:01 +0200

    Add metadata prefix to all scopes

--- lib/auxilium/concerns/metadata_scoped.rb
+++ lib/auxilium/concerns/metadata_scoped.rb
@@ -38,18 +38,33 @@ module Auxilium
           result
         }
 
-        scope :contains, lambda { |name, value|
+        scope :metadata_contains, lambda { |name, value|
           where('(metadata->>:name)::jsonb ? :value', name: name, value: value)
         }
 
-        scope :contains_or_blank, lambda { |name, value|
+        scope :contains, lambda { |name, value|
+          ActiveSupport::Deprecation.warn('Use metdata_contains instead')
+          metadata_contains(name, value)
+        }
+
+        scope :metadata_contains_or_blank, lambda { |name, value|
           where('(metadata->>:name)::jsonb ? :value OR metadata->>:name IS NULL', name: name, value: value)
         }
 
-        scope :contains_or_blank_exclusive, lambda { |name, value|
+        scope :contains_or_blank, lambda { |name, value|
+          ActiveSupport::Deprecation.warn('Use metdata_contains_or_blank instead')
+          metadata_contains_or_blank(name, value)
+        }
+
+        scope :metadata_contains_or_blank_exclusive, lambda { |name, value|
           result = where('(metadata->>:name)::jsonb ? :value', name: name, value: value)
           result = where('metadata->>:name IS NULL', name: name) if result.none?
           result
+        }
+
+        scope :contains_or_blank_exclusive, lambda { |name, value|
+          ActiveSupport::Deprecation.warn('Use metdata_contains_or_blank_exclusive instead')
+          metadata_contains_or_blank_exclusive(name, value)
         }
       end
     end
