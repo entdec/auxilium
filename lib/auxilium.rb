9b20b411ec5a7ae5df75bef162b2b95080964d7c
Andre Meij <andre@itsmeij.com>
2020-06-08 10:31:45 +0200

    Add Uuidmod

--- lib/auxilium.rb
+++ lib/auxilium.rb
@@ -3,6 +3,7 @@ require "auxilium/version"
 require "active_support"
 
 require "auxilium/easy_crypt"
+require "auxilium/uuid_modifier"
 require "auxilium/grape"
 require "auxilium/grape/parameter_filter"
 
