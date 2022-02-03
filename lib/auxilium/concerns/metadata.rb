5fe040bc5b4750ac035e911228a0361cf96fc21c
Mark Cornelissen <mark@boxture.com>
2022-02-03 15:27:07 +0100

    Adds the metadata concern

---
+++ lib/auxilium/concerns/metadata.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Auxilium
+  module Metadata
+    extend ActiveSupport::Concern
+
+    def metadata
+      attributes['metadata'] && HashWithIndifferentAccess.new(attributes['metadata'])
+    end
+
+    def metadata_yaml=(yaml)
+      write_attribute :metadata, YAML.safe_load(yaml.gsub("\t", '  '))
+    end
+
+    def metadata_yaml
+      return '' if metadata.nil? || metadata.empty?
+
+      YAML.dump(attributes['metadata'])
+    end
+  end
+end
