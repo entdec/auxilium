cca9192a411dea3f4ced547c0a6d5a0600e31ee5
Tom de Grunt <tom@degrunt.nl>
2023-12-18 13:13:29 +0100

    We might not have Devise soon

--- lib/auxilium/responders/continue_responder.rb
+++ lib/auxilium/responders/continue_responder.rb
@@ -7,7 +7,7 @@ module Auxilium
     protected
 
     def navigation_location
-      return options[:location] if options[:location] && (defined?(Devise) && controller.is_a?(Devise::SessionsController) || controller.is_a?(Devise::PasswordsController))
+      return options[:location] if options[:location] && (defined?(Devise) && (controller.is_a?(Devise::SessionsController) || controller.is_a?(Devise::PasswordsController)))
       return options[:location] if controller.params[:commit] == 'continue' && options[:location]
       return options[:collection_location].call if %w[save commit].include?(controller.params[:commit]) && options[:collection_location]
       return resource_location if controller.params[:commit] == 'continue'
